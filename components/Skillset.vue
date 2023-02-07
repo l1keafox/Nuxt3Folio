dfce076144c4fda0b1f21ffe7d7cd48ac618cf47
Raymond Lewis <raymond.ed.lewis@gmail.com>
2023-02-07 16:29:31 -0700

    fixing some issues

--- components/Skillset.vue
+++ components/Skillset.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="absolute w-full flex flex-col items-center justify-center md:top-[46rem] top-[43rem]">
         <p className="text-5xl block relative text-center font-ubuntu  dark:text-slate-400">
-            my skillset
+            My Skillset
           </p>
           <br/>
         <div class="flex items-center justify-center flex-wrap px-5">
             <Icon name="ion:logo-html5"/>
             <Icon name="ion:logo-react"/>
+            <Icon name="ion:logo-jest"/>
             <Icon name="akar-icons:bootstrap-fill"/>
             <Icon name="vscode-icons:file-type-mysql"/>
             <Icon name="logos:mongodb-icon"/>
