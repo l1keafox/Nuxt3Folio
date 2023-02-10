6b1a65251fccf6e527c5ac12d0747f9228840443
Raymond Lewis <raymond.ed.lewis@gmail.com>
2023-02-09 21:59:59 -0700

    pinia added and working

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,18 @@
 <template>
+  <nav v-if="logged.in" class="fixed z-50">
+    <NuxtLink to ="/#resume">Home</NuxtLink>
+    <NuxtLink to ="/#resume">Home</NuxtLink>
+    <NuxtLink to ="/#resume">Home</NuxtLink>
+    <NuxtLink to ="/#resume">Home</NuxtLink>
+    <NuxtLink to ="/#resume">Home</NuxtLink>
+    <NuxtLink to ="/#resume">Home</NuxtLink>
+  </nav>
   <main>
     <slot />
   </main>
 </template>
+
+<script setup>
+const logged  = useLogged();
+
+</script>
