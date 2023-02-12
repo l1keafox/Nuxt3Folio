79da9211c89f5567c47d077687970b7a32bc0f07
Raymond Lewis <raymond.ed.lewis@gmail.com>
2023-02-12 13:01:18 -0700

    lazy validate email

--- components/MessageMe.vue
+++ components/MessageMe.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col w-screen absolute top-[0rem] md:top-[2rem] lg:top-[62rem]  xl:top-[40rem] z-10 py-8 gap-x-2 gap-y-1 items-center justify-center font-josefin">    <h1> Message Me!</h1>
     <form @submit.prevent="submit" class="flex flex-col w-[30rem]">
       <InputField label="name" v-model="name" :error="errors.name"> </InputField>
-      <InputField label="email" v-model="email" :error="errors.email"> </InputField>
+      <InputField label="email" :error="errors.email" :modelValue="email" @change="handleChange"> </InputField>
       <TextField label="message" v-model="message" :error="errors.message" class="h-[10rem]" />
       <MyButton type="submit" text="Submit" hover="bg-red-700" color="bg-red-500"/>
     </form>
@@ -29,7 +29,7 @@ const validationSchema = object({
     message: string()
 })
 
-const {handleSubmit, errors} = useForm({
+const {handleSubmit, errors, setFieldValue} = useForm({
     validationSchema,
     initialValues:{
       email:null,
@@ -42,6 +42,9 @@ const { value: email } = useField('email')
 const { value: name } = useField('name')
 const { value: message } = useField('message')
 
+const handleChange = (event) =>{
+  setFieldValue('email',event.target.value)
+}
 const submit = handleSubmit(values => {
       console.log('submit', values)
       console.log(errors.name);
