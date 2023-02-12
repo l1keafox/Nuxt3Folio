334a90b13c646e08e3099caa569a69947f250a6d
Raymond Lewis <raymond.ed.lewis@gmail.com>
2023-02-12 11:40:19 -0700

    Created Input component

---
+++ components/InputField.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script setup>
+const { label, modelValue , error } = defineProps({
+  label: {
+    type: String,
+    default: ''
+  },
+  modelValue: {
+    type: [String,Number],
+    default: ''
+  },
+  error: {
+    type: String,
+    default: ''
+  },
+})
+</script>
