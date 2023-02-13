eb42205dbda1f5da8c6ffcd9cfd46182aad01941
Raymond Lewis <raymond.ed.lewis@gmail.com>
2023-02-13 11:09:32 -0700

    emailjs works

--- components/MessageMe.vue
+++ components/MessageMe.vue
@@ -1,33 +1,34 @@
 <template>
   <div id="messageMe" class="flex flex-col text-3xl w-full absolute  z-10 py-8 gap-x-2 gap-y-1 items-center justify-center font-josefin p-2">
     <h1> Message Me!</h1>
-    <form @submit.prevent="submit" class="flex flex-col w-full md:w-[40rem]  text-xl">
-      <InputField label="name" v-model="name" :error="errors.name"> </InputField>
-      <InputField label="email" :error="errors.email" :modelValue="email" @change="handleChange"> </InputField>
-      <TextField label="message" v-model="message" :error="errors.message" class="h-[10rem]" />
+    <form ref="form" id="form" @submit.prevent="submit" class="flex flex-col w-full md:w-[40rem]  text-xl">
+      <InputField label="name" id="from_name" name="from_name" v-model="name" :error="errors.name"> </InputField>
+      <InputField label="email" id="reply_to" name="reply_to" :error="errors.email" :modelValue="email" @change="handleChange"> </InputField>
+      <TextField label="message" id="message" name="message" v-model="message" :error="errors.message" class="h-[10rem]" />
       <MyButton type="submit" text="Submit" hover="bg-red-700" color="bg-red-500"/>
     </form>
   </div>
 </template>
 
 <script setup>
+import emailjs from "@emailjs/browser";
 import { useField, useForm } from 'vee-validate'
 import { object, string } from 'yup'
+const form = ref(null)
 
 const validationSchema = object({
     email: string().email("Field should contain a valid e-mail").max(255).required("E-mail is required"),
-    // email: value => {
-    //     if (!value) return 'This field is required'
-
-    //     const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    //     if (!regex.test(String(value).toLowerCase())) {
-    //       return 'Please enter a valid email address'
-    //     }
-
-    //     return true
-    //   },
     name: string(),
     message: string()
+})
+onMounted(()=>{
+  watchEffect(() => {
+  if (form.value) {
+    form.value.focus()
+  } else {
+    // not mounted yet, or the element was unmounted (e.g. by v-if)
+  }
+})
 })
 
 const {handleSubmit, errors, setFieldValue} = useForm({
@@ -47,10 +48,14 @@ const handleChange = (event) =>{
   setFieldValue('email',event.target.value)
 }
 const submit = handleSubmit(values => {
-      console.log('submit', values)
-      console.log(errors.name);
-      console.log(errors.email);
-      console.log(errors.message);
+      let ditto = document.getElementById('form');
+      let result = emailjs.sendForm(
+      "service_3l5kfbj",
+      "template_7630s4a",
+      ditto,
+      "b3A_HBPJ0CCCA_D0d"
+    )
+    alert("Thank you for sendin me a message,I will get back too you asap");
 })
 
 </script>
